214be0648813d81e09f7bd532a513fed3b1a7d0f
Zach Simonson <zsimonson26@gmail.com>
2016-01-11 12:53:11 -0800

    created stores objects with given properties

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
 //my js will be in here
+var pike = {
+  minHourlyTraffic: 14,
+  maxHourlyTraffic: 55,
+  cupsPerCust: 1.2,
+  poundsPerCust: 3.7
+};
+var capHill = {
+  minHourlyTraffic: 32,
+  maxHourlyTraffic: 48,
+  cupsPerCust: 3.2,
+  poundsPerCust:0.4
+};
+var seaPubLib = {
+  minHourlyTraffic: 49,
+  maxHourlyTraffic: 48,
+  cupsPerCust: 2.6,
+  poundsPerCust: 0.2
+};
+var southLakeUnion = {
+  minHourlyTraffic: 49,
+  maxHourlyTraffic: 75,
+  cupsPerCust: 1.3,
+  poundsPerCust: 3.7
+};
+var seaTacAirport = {
+  minHourlyTraffic: 68,
+  maxHourlyTraffic: 124,
+  cupsPerCust: 1.1,
+  poundsPerCust: 2.7
+};
+var websiteSales = {
+  minHourlyTraffic: 3,
+  maxHourlyTraffic: 6,
+  cupsPerCust: 0,
+  poundsPerCust: 6.7
+};
