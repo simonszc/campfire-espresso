2a96993fb9a0b5d37076433e38421b6378109b7f
Zach Simonson <zsimonson26@gmail.com>
2016-01-14 10:19:32 -0800

    changed data output to match new user story

--- js/app.js
+++ js/app.js
@@ -7,10 +7,11 @@ function Kiosk (locName, minTraffic, maxTraffic, cupsPer, poundsPer) {
   this.hourlyTraffic = [];
   this.totalTrafficCounter = 0;
   this.totalTraffic = [];
+  this.soldCupsInCups = [];
   this.hourlyCupsInCups = [];
-  this.hourlyCupsInPounds = [];
-  this.hourlyToGoInPounds = [];
-  this.hourlyPoundsSoldTotal = [];
+  this.soldCupsInPounds = [];
+  this.soldToGoInPounds = [];
+  this.soldPoundsTotal = [];
 };
 Kiosk.prototype.calculateHourlyTraffic = function() {
   for (var i = 0; i < hoursOpen.length; i++) {
@@ -25,14 +26,16 @@ Kiosk.prototype.calculateHourlySales = function (){
   var totalToGoInPounds = 0;
   var totalPoundsSold = 0;
   for(var i = 0; i < hoursOpen.length; i++){
+    CupsInCups = this.hourlyTraffic[i] * this.cupsPerCust;
+    this.hourlyCupsInCups.push(CupsInCups);
     totalCupsInCups += this.hourlyTraffic[i] * this.cupsPerCust;
-    this.hourlyCupsInCups.push(totalCupsInCups);
-    totalCupsInPounds = this.hourlyCupsInCups[i] * 0.05;
-    this.hourlyCupsInPounds.push(totalCupsInPounds);
+    this.soldCupsInCups.push(totalCupsInCups);
+    totalCupsInPounds = this.soldCupsInCups[i] * 0.05;
+    this.soldCupsInPounds.push(totalCupsInPounds);
     totalToGoInPounds += this.hourlyTraffic[i] * this.poundsPerCust;
-    this.hourlyToGoInPounds.push(totalToGoInPounds);
+    this.soldToGoInPounds.push(totalToGoInPounds);
     totalPoundsSold += this.hourlyTraffic[i] * this.cupsPerCust * 0.05 + this.hourlyTraffic[i] * this.poundsPerCust;
-    this.hourlyPoundsSoldTotal.push(totalPoundsSold.toFixed(1));
+    this.soldPoundsTotal.push(totalPoundsSold.toFixed(1));
   }
 };
 var pike = new Kiosk('Pike Place Market', 14, 55, 1.2, 3.7);
@@ -48,7 +51,7 @@ for (var i = 0; i < allKiosks.length; i++) {
   allKiosks[i].calculateHourlySales();
 }
 
-Kiosk.prototype.displayTable = function() {
+Kiosk.prototype.displayPoundsTable = function() {
   var tableEl = document.createElement('table');
   //first row is special
     var firstRow = document.createElement('tr');
@@ -69,12 +72,74 @@ Kiosk.prototype.displayTable = function() {
       newRow.appendChild(displayHour);
        for (var i = 0; i < allKiosks.length; i++){
          var displayPounds = document.createElement('td');
-         displayPounds.textContent = allKiosks[i].hourlyPoundsSoldTotal[hour];
+         displayPounds.textContent = allKiosks[i].soldPoundsTotal[hour];
          newRow.appendChild(displayPounds);
        }
       tableEl.appendChild(newRow);
     }
-  document.getElementById('tableDisplay').appendChild(tableEl);
+  document.getElementById('poundsDisplay').appendChild(tableEl);
 };
 
-websiteSales.displayTable();
+websiteSales.displayPoundsTable();
+
+Kiosk.prototype.displayCupsTable = function() {
+  var tableEl = document.createElement('table');
+  //first row is special
+    var firstRow = document.createElement('tr');
+      var firstRowFirstCell = document.createElement('th');
+      firstRowFirstCell.textContent = '';
+      firstRow.appendChild(firstRowFirstCell);
+      for (var i = 0; i < allKiosks.length; i++) {
+        var locationDisplay = document.createElement('th');
+        locationDisplay.textContent = allKiosks[i].locationName;
+        firstRow.appendChild(locationDisplay);
+      }
+    tableEl.appendChild(firstRow);
+    //rest of rows
+    for (var hour = 0; hour < hoursOpen.length; hour++) {
+      var newRow = document.createElement('tr');
+      var displayHour = document.createElement('th');
+      displayHour.textContent = hoursOpen[hour];
+      newRow.appendChild(displayHour);
+       for (var i = 0; i < allKiosks.length; i++){
+         var displayCups = document.createElement('td');
+         displayCups.textContent = allKiosks[i].hourlyCupsInCups[hour].toFixed(1);
+         newRow.appendChild(displayCups);
+       }
+      tableEl.appendChild(newRow);
+    }
+  document.getElementById('cupsDisplay').appendChild(tableEl);
+};
+
+websiteSales.displayCupsTable();
+
+Kiosk.prototype.displayTrafficTable = function() {
+  var tableEl = document.createElement('table');
+  //first row is special
+    var firstRow = document.createElement('tr');
+      var firstRowFirstCell = document.createElement('th');
+      firstRowFirstCell.textContent = '';
+      firstRow.appendChild(firstRowFirstCell);
+      for (var i = 0; i < allKiosks.length; i++) {
+        var locationDisplay = document.createElement('th');
+        locationDisplay.textContent = allKiosks[i].locationName;
+        firstRow.appendChild(locationDisplay);
+      }
+    tableEl.appendChild(firstRow);
+    //rest of rows
+    for (var hour = 0; hour < hoursOpen.length; hour++) {
+      var newRow = document.createElement('tr');
+      var displayHour = document.createElement('th');
+      displayHour.textContent = hoursOpen[hour];
+      newRow.appendChild(displayHour);
+       for (var i = 0; i < allKiosks.length; i++){
+         var displayTraffic = document.createElement('td');
+         displayTraffic.textContent = allKiosks[i].hourlyTraffic[hour];
+         newRow.appendChild(displayTraffic);
+       }
+      tableEl.appendChild(newRow);
+    }
+  document.getElementById('trafficDisplay').appendChild(tableEl);
+};
+
+websiteSales.displayTrafficTable();
